4fb5ece891f51794c7d3ef13c15db23b9a25385c
catse2000 <catse2000@yahoo.com>
2020-11-01 22:55:16 -0600

    added CRUD operations for Post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User'); // import the User model from the "models" folder
 const Post = require('./Post'); // import the Post model from the "models" folder
 
+// create associations between models
+User.hasMany(Post, { // this relationship shows that one User can have many Posts
+    foreignKey: 'user_id' // links relationship between User's "id" and Post's "user_id"
+});
+
+Post.belongsTo(User, { // this relationship shows that a Post can only have one User
+    foreignKey: 'user_id' // links relationship between User's "id" and Post's "user_id"
+});
+
 module.exports = { User, Post }; // export models so other parts of the application can access them
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); // import the express server
 
 const userRoutes = require('./user-routes.js'); // import the user-routes
+const postRoutes = require('./post-routes.js'); // import the post-routes
 
 router.use('/users', userRoutes); // use the user-routes and adds the prefix /users to those routes
+router.use('/posts', postRoutes); // use the post-routes and adds the previx /posts to those routes
 
 module.exports = router; // export for other parts of the application to see and use
