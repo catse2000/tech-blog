ce6e288c127a9e9bc188b38bcff50b7e2474ade7
catse2000 <catse2000@yahoo.com>
2020-11-02 19:23:43 -0600

    add ability to login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,7 +7,15 @@ router.get('/', (req, res) => {
     User.findAll({
         attributes: { exclude: ['password'] }
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => { // store session cookie using user_id, username, and boolean loggedIn
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -67,10 +75,9 @@ router.post('/', (req, res) => {
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
-            email: req.body.email
+            username: req.body.username
         }
-    })
-    .then(dbUserData => {
+    }).then(dbUserData => {
         if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
@@ -79,14 +86,32 @@ router.post('/login', (req, res) => {
         const validPassword = dbUserData.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password' });
+            res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
 });
 
+// POST route used to log a user out of their account
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 // PUT route used to update an entry in the USERS table using the primary key ID to define which entry to update
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const routes = require('./controllers'); // import the routes folder
 const sequelize = require('./config/connection'); // import the connection file for the database
 const path = require('path'); //import public folder to access javascript and stylesheets
 const exphbs = require('express-handlebars')// import express-handlebars as the website engine
-const hbs = exphbs.create({});
+const hbs = exphbs.create({}); //create new handlebars engine
 
 const app = express(); // initialize the express server
 const PORT = process.env.PORT || 3001; // if port 3001 is not available use one provided by the service hosting the application
+
+const session = require('express-session'); // import connection to create and save sessions in browser
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // stores the sessions created by express-session into our database
+
+const sess = { // creates a cookie and and stores it in the database
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true, 
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess)); // stores the cookie in the browser and starts a session
 
 // express middleware
 app.use(express.json());
@@ -17,8 +32,9 @@ app.use(express.static(path.join(__dirname, 'public'))); // points express to th
 app.use(routes);
 
 // set up handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine); // use handlebars
+app.set('view engine', 'handlebars'); // set up engine
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => { // make sure database is started first before starting up the server. ".then" makes this a promise!
